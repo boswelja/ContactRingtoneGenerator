56c2ed6677910a022ce6f29771e7b8ca6919974c
Jack Boswell (boswelja) <boswelja@outlook.com>
2020-03-27 12:24:42 +1300

    Make use of view binding for MainActivity.kt and activity_main.xml

--- app/src/main/java/com/boswelja/contactringtonegenerator/ui/MainActivity.kt
+++ app/src/main/java/com/boswelja/contactringtonegenerator/ui/MainActivity.kt
@@ -7,21 +7,13 @@ import android.os.Bundle
 import android.util.Log
 import android.view.View
 import android.widget.AdapterView
-import android.widget.RelativeLayout
 import android.widget.SeekBar
-import androidx.appcompat.widget.AppCompatCheckBox
-import androidx.appcompat.widget.AppCompatEditText
-import androidx.appcompat.widget.AppCompatImageView
-import androidx.appcompat.widget.AppCompatSeekBar
-import androidx.appcompat.widget.AppCompatSpinner
-import androidx.appcompat.widget.AppCompatTextView
-import androidx.appcompat.widget.LinearLayoutCompat
 import androidx.core.widget.doOnTextChanged
+import androidx.databinding.DataBindingUtil
 import com.boswelja.contactringtonegenerator.R
 import com.boswelja.contactringtonegenerator.contacts.Contact
+import com.boswelja.contactringtonegenerator.databinding.ActivityMainBinding
 import com.boswelja.contactringtonegenerator.ringtonegen.RingtoneGenerator
-import com.google.android.material.button.MaterialButton
-import com.google.android.material.textfield.TextInputLayout
 import java.util.Locale
 
 class MainActivity :
@@ -30,31 +22,17 @@ class MainActivity :
 
     private lateinit var ringtoneGenerator: RingtoneGenerator
 
-    private lateinit var contactSelectorView: RelativeLayout
-    private lateinit var contactsSelectedView: AppCompatTextView
-
-    private lateinit var useNicknamesView: AppCompatCheckBox
-
-    private lateinit var voicePickerView: LinearLayoutCompat
-    private lateinit var voicePickerSpinner: AppCompatSpinner
-
-    private lateinit var voiceSpeedSlider: AppCompatSeekBar
-    private lateinit var voiceSpeedText: AppCompatTextView
-    private lateinit var voiceSpeedReset: AppCompatImageView
-
-    private lateinit var messageTextLayout: TextInputLayout
-    private lateinit var messageTextField: AppCompatEditText
-
-    private lateinit var generateButton: MaterialButton
-    private lateinit var previewButton: MaterialButton
-
     private lateinit var contactPickerDialog: ContactPickerDialog
+
+    private lateinit var binding: ActivityMainBinding
 
     override fun onContactsSelected(selectedContacts: List<Contact>) {
         val selectedContactCount = selectedContacts.count()
-        contactsSelectedView.text = resources.getQuantityString(R.plurals.selected_contacts_summary, 0, selectedContactCount)
         ringtoneGenerator.setContacts(selectedContacts)
-        generateButton.isEnabled = ringtoneGenerator.isReady && selectedContactCount > 0
+        binding.apply {
+            contactSelectorSummary.text = resources.getQuantityString(R.plurals.selected_contacts_summary, 0, selectedContactCount)
+            generateButton.isEnabled = ringtoneGenerator.isReady && selectedContactCount > 0
+        }
     }
 
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -63,35 +41,33 @@ class MainActivity :
         contactPickerDialog = ContactPickerDialog()
         contactPickerDialog.dialogEventListeners.add(this)
 
-        setContentView(R.layout.activity_main)
-        initViews()
+        binding = DataBindingUtil.setContentView(this, R.layout.activity_main)
 
-        generateButton.apply {
-            setOnClickListener {
-                val selectedContacts = contactPickerDialog.getSelectedContacts()
-                if (!selectedContacts.isNullOrEmpty()) {
-                    ringtoneGenerator.setContacts(selectedContacts)
-                    ringtoneGenerator.generate()
+        binding.apply {
+            generateButton.apply {
+                setOnClickListener {
+                    val selectedContacts = contactPickerDialog.getSelectedContacts()
+                    if (!selectedContacts.isNullOrEmpty()) {
+                        ringtoneGenerator.setContacts(selectedContacts)
+                        ringtoneGenerator.generate()
+                    }
                 }
             }
-        }
 
-        useNicknamesView.setOnCheckedChangeListener { _, isChecked ->
-            ringtoneGenerator.useNicknames = isChecked
-            contactPickerDialog.setUseNicknames(isChecked)
-        }
-
-        previewButton.apply {
-            setOnClickListener {
-                ringtoneGenerator.preview()
+            useNicknamesCheckbox.setOnCheckedChangeListener { _, isChecked ->
+                ringtoneGenerator.useNicknames = isChecked
+                contactPickerDialog.setUseNicknames(isChecked)
             }
+
+            previewButton.setOnClickListener { ringtoneGenerator.preview() }
         }
+
 
         ringtoneGenerator = RingtoneGenerator(this)
         ringtoneGenerator.addEventListener(object : RingtoneGenerator.EventListener {
             override fun onReady() {
                 setupVoicePickerSpinner()
-                previewButton.isEnabled = true
+                binding.previewButton.isEnabled = true
             }
         })
         ringtoneGenerator.init()
@@ -126,30 +102,10 @@ class MainActivity :
         }
     }
 
-    private fun initViews() {
-        contactsSelectedView = findViewById(R.id.contact_selector_summary)
-        contactSelectorView = findViewById(R.id.contact_selector)
-
-        useNicknamesView = findViewById(R.id.use_nicknames_checkbox)
-
-        voicePickerView = findViewById(R.id.voice_picker_view)
-        voicePickerSpinner = findViewById(R.id.voice_picker_spinner)
-
-        voiceSpeedText = findViewById(R.id.voice_speed_slider_text)
-        voiceSpeedSlider = findViewById(R.id.voice_speed_slider)
-        voiceSpeedReset = findViewById(R.id.voice_speed_reset)
-
-        messageTextLayout = findViewById(R.id.message_input_layout)
-        messageTextField = findViewById(R.id.message_input_field)
-
-        generateButton = findViewById(R.id.generate_button)
-        previewButton = findViewById(R.id.preview_button)
-    }
-
     private fun setupContactsPicker() {
-        contactsSelectedView.text = resources.getQuantityString(R.plurals.selected_contacts_summary, 0, contactPickerDialog.getSelectedContacts().count())
-        contactSelectorView.apply {
-            setOnClickListener {
+        binding.apply {
+            contactSelectorSummary.text = resources.getQuantityString(R.plurals.selected_contacts_summary, 0, contactPickerDialog.getSelectedContacts().count())
+            contactSelector.setOnClickListener {
                 if (checkSelfPermission(Manifest.permission.READ_CONTACTS) != PackageManager.PERMISSION_GRANTED) {
                     requestPermissions(arrayOf(Manifest.permission.READ_CONTACTS, Manifest.permission.WRITE_CONTACTS), CONTACT_PICKER_PERMISSION_REQUEST_CODE)
                 } else {
@@ -160,23 +116,23 @@ class MainActivity :
     }
 
     private fun setupMessageTextField() {
-        messageTextField.apply {
-            doOnTextChanged { text, _, _, _ ->
+        binding.apply {
+            messageInputField.doOnTextChanged { text, _, _, _ ->
                 if (ringtoneGenerator.setRingtoneMessage(text.toString())) {
-                    messageTextLayout.isErrorEnabled = false
+                    messageInputLayout.isErrorEnabled = false
                 } else {
-                    messageTextLayout.isErrorEnabled = true
-                    messageTextLayout.error = "Message should contain '%NAME' to use the name of the contact."
+                    messageInputLayout.isErrorEnabled = true
+                    messageInputLayout.error = "Message should contain '%NAME' to use the name of the contact."
                 }
             }
         }
-        ringtoneGenerator.setRingtoneMessage(messageTextField.text.toString())
+        ringtoneGenerator.setRingtoneMessage(binding.messageInputField.text.toString())
     }
 
     private fun setupVoicePickerSpinner() {
         val voices = ringtoneGenerator.getAvailableVoices(Locale.getDefault())!!
         if (voices.isNotEmpty()) {
-            voicePickerSpinner.apply {
+            binding.voicePickerSpinner.apply {
                 Log.d("VoicePickerSpinner", "Found ${voices.count()} voices")
                 adapter = VoiceSpinnerAdapter(context, voices)
                 onItemSelectedListener = object : AdapterView.OnItemSelectedListener {
@@ -195,17 +151,17 @@ class MainActivity :
                 setSelection(0)
                 visibility = View.VISIBLE
             }
-            voicePickerView.visibility = View.VISIBLE
+            binding.voicePickerView.visibility = View.VISIBLE
         } else {
-            voicePickerView.visibility = View.GONE
+            binding.voicePickerView.visibility = View.GONE
         }
 
     }
 
     private fun setupVoiceSpeedSlider() {
         voiceSpeedSliderChange(1.0f)
-        voiceSpeedSlider.apply {
-            setOnSeekBarChangeListener(object: SeekBar.OnSeekBarChangeListener {
+        binding.apply {
+            voiceSpeedSlider.setOnSeekBarChangeListener(object: SeekBar.OnSeekBarChangeListener {
                 override fun onProgressChanged(
                     seekBar: SeekBar?,
                     progress: Int,
@@ -216,21 +172,19 @@ class MainActivity :
                 }
 
                 override fun onStartTrackingTouch(seekBar: SeekBar?) {}
-
                 override fun onStopTrackingTouch(seekBar: SeekBar?) {}
 
             })
-        }
-        voiceSpeedReset.apply {
-            setOnClickListener {
+            voiceSpeedReset.setOnClickListener {
                 voiceSpeedSlider.progress = 5
                 voiceSpeedSliderChange(1.0f)
             }
         }
+
     }
 
     private fun voiceSpeedSliderChange(actualMultiplier: Float) {
-        voiceSpeedText.text = getString(R.string.speech_rate_multiplier_text, actualMultiplier)
+        binding.voiceSpeedSliderText.text = getString(R.string.speech_rate_multiplier_text, actualMultiplier)
         ringtoneGenerator.setSpeechRate(actualMultiplier)
     }
 
