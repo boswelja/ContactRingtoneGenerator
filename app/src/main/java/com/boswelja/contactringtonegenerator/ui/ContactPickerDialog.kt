2233739b83dd56b902c1dd5c23561aa92488b767
Jack Boswell (boswelja) <boswelja@outlook.com>
2020-03-27 12:48:27 +1300

    Use view binding for ContactPickerDialog.kt and contact_picker_dialog.xml

--- app/src/main/java/com/boswelja/contactringtonegenerator/ui/ContactPickerDialog.kt
+++ app/src/main/java/com/boswelja/contactringtonegenerator/ui/ContactPickerDialog.kt
@@ -2,25 +2,25 @@ package com.boswelja.contactringtonegenerator.ui
 
 import android.app.Dialog
 import android.os.Bundle
+import android.util.Log
+import android.view.LayoutInflater
 import android.view.View
-import android.widget.ProgressBar
 import androidx.appcompat.app.AlertDialog
+import androidx.databinding.DataBindingUtil
 import androidx.fragment.app.DialogFragment
 import androidx.fragment.app.FragmentManager
 import androidx.recyclerview.widget.LinearLayoutManager
-import androidx.recyclerview.widget.RecyclerView
 import com.boswelja.contactringtonegenerator.R
 import com.boswelja.contactringtonegenerator.contacts.Contact
 import com.boswelja.contactringtonegenerator.contacts.ContactManager
+import com.boswelja.contactringtonegenerator.databinding.ContactPickerDialogBinding
 
 class ContactPickerDialog : DialogFragment() {
 
-    private var contactsRecyclerView: RecyclerView? = null
     private var dialog: AlertDialog? = null
     private var useNicknames: Boolean = true
 
-    private lateinit var noContactsView: View
-    private lateinit var loadingIndicator: ProgressBar
+    private var binding: ContactPickerDialogBinding? = null
 
     val dialogEventListeners = ArrayList<DialogEventListener>()
 
@@ -42,47 +42,51 @@ class ContactPickerDialog : DialogFragment() {
     }
 
     private fun createDialog() {
-        val view = View.inflate(context, R.layout.contact_picker_dialog, null)
-        contactsRecyclerView = view.findViewById<RecyclerView>(R.id.contacts_recyclerview).apply {
+        binding = DataBindingUtil.inflate(LayoutInflater.from(context),
+            R.layout.contact_picker_dialog, null, false)
+
+        binding!!.contactsRecyclerview.apply {
             layoutManager = LinearLayoutManager(context, LinearLayoutManager.VERTICAL, false)
             adapter = ContactPickerAdapter().apply {
                 setUseNicknames(useNicknames)
             }
         }
-        noContactsView = view.findViewById(R.id.no_contacts_view)
-        loadingIndicator = view.findViewById(R.id.loading_indicator)
         dialog = AlertDialog.Builder(context!!).apply {
             setTitle(R.string.contact_picker_dialog_title)
             setPositiveButton(R.string.dialog_button_done) { _, _ ->
                 for (listener in dialogEventListeners) {
-                    listener.onContactsSelected((contactsRecyclerView!!.adapter as ContactPickerAdapter).selectedContacts)
+                    listener.onContactsSelected(
+                        (binding!!.contactsRecyclerview.adapter as ContactPickerAdapter).selectedContacts)
                 }
             }
-            setView(view)
+            setView(binding!!.root)
         }.create()
     }
 
     private fun setLoading(isLoading: Boolean) {
-        if (isLoading) {
-            loadingIndicator.visibility = View.VISIBLE
-            contactsRecyclerView!!.visibility = View.GONE
-            noContactsView.visibility = View.GONE
-        } else {
-            loadingIndicator.visibility = View.GONE
-            if ((contactsRecyclerView!!.adapter as ContactPickerAdapter).itemCount > 0) {
+        binding!!.apply {
+            if (isLoading) {
+                loadingIndicator.visibility = View.VISIBLE
+                contactsRecyclerview.visibility = View.GONE
                 noContactsView.visibility = View.GONE
-                contactsRecyclerView!!.visibility = View.VISIBLE
             } else {
-                noContactsView.visibility = View.VISIBLE
-                contactsRecyclerView!!.visibility = View.GONE
+                loadingIndicator.visibility = View.GONE
+                if ((contactsRecyclerview.adapter as ContactPickerAdapter).itemCount > 0) {
+                    noContactsView.visibility = View.GONE
+                    contactsRecyclerview.visibility = View.VISIBLE
+                } else {
+                    noContactsView.visibility = View.VISIBLE
+                    contactsRecyclerview.visibility = View.GONE
+                }
             }
         }
+
     }
 
     private fun updateContacts() {
         if (wantsContactUpdate) {
             wantsContactUpdate = false
-            (contactsRecyclerView!!.adapter as ContactPickerAdapter)
+            (binding!!.contactsRecyclerview.adapter as ContactPickerAdapter)
                 .setContacts(ContactManager.getContacts(context!!))
         }
     }
@@ -92,18 +96,13 @@ class ContactPickerDialog : DialogFragment() {
     }
 
     fun getSelectedContacts(): List<Contact> {
-        return if (contactsRecyclerView != null) {
-            (contactsRecyclerView?.adapter as ContactPickerAdapter).selectedContacts
-        } else {
-            ArrayList()
-        }
+        return (binding?.contactsRecyclerview?.adapter as ContactPickerAdapter?)?.selectedContacts
+            ?: ArrayList()
     }
 
     fun setUseNicknames(useNicknames: Boolean) {
         this.useNicknames = useNicknames
-        if (contactsRecyclerView != null) {
-            (contactsRecyclerView!!.adapter as ContactPickerAdapter).setUseNicknames(useNicknames)
-        }
+        (binding?.contactsRecyclerview?.adapter as ContactPickerAdapter?)?.setUseNicknames(useNicknames)
     }
 
     interface DialogEventListener {
