6a7568e755df0a60fa675cb179d7357381d16014
Jack Boswell (boswelja) <boswelja@outlook.com>
2020-05-28 16:29:35 +1200

    Add a toolbar to show a back button where possible

--- app/src/main/java/com/boswelja/contactringtonegenerator/ui/MainActivity.kt
+++ app/src/main/java/com/boswelja/contactringtonegenerator/ui/MainActivity.kt
@@ -2,6 +2,10 @@ package com.boswelja.contactringtonegenerator.ui
 
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
+import androidx.navigation.findNavController
+import androidx.navigation.ui.AppBarConfiguration
+import androidx.navigation.ui.setupWithNavController
+import com.boswelja.contactringtonegenerator.R
 import com.boswelja.contactringtonegenerator.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
@@ -12,5 +16,13 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
+        setSupportActionBar(binding.toolbar)
+        supportActionBar?.apply {
+            setDisplayShowTitleEnabled(false)
+        }
+
+        val navController = findNavController(R.id.nav_host_fragment_container)
+        val appBarConfiguration = AppBarConfiguration(navController.graph)
+        binding.toolbar.setupWithNavController(navController, appBarConfiguration)
     }
 }
