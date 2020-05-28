e0aeff2739a41812a9d0d0dbc9864331756cea31
Jack Boswell (boswelja) <boswelja@outlook.com>
2020-05-28 18:47:50 +1200

    Add a VoicePickerFragment.kt

---
+++ app/src/main/java/com/boswelja/contactringtonegenerator/ui/VoicePickerFragment.kt
@@ -0,0 +1,22 @@
+package com.boswelja.contactringtonegenerator.ui
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.fragment.app.Fragment
+import com.boswelja.contactringtonegenerator.databinding.FragmentVoicePickerBinding
+
+class VoicePickerFragment : Fragment() {
+
+    private lateinit var binding: FragmentVoicePickerBinding
+
+    override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
+        binding = FragmentVoicePickerBinding.inflate(inflater, container, false)
+        return binding.root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+
+    }
+}
