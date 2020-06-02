d8f0a18d3aeb60d36a5429a73b4c458ff4bddd20
Jack Boswell (boswelja) <boswelja@outlook.com>
2020-06-02 15:08:09 +1200

    Create an empty layout for RingtoneCreatorFragment.kt

--- app/src/main/java/com/boswelja/contactringtonegenerator/ui/ringtonecreator/RingtoneCreatorFragment.kt
+++ app/src/main/java/com/boswelja/contactringtonegenerator/ui/ringtonecreator/RingtoneCreatorFragment.kt
@@ -1,6 +1,18 @@
 package com.boswelja.contactringtonegenerator.ui.ringtonecreator
 
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
 import androidx.fragment.app.Fragment
+import com.boswelja.contactringtonegenerator.databinding.FragmentRingtoneCreatorBinding
 
 class RingtoneCreatorFragment : Fragment() {
+
+    private lateinit var binding: FragmentRingtoneCreatorBinding
+
+    override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
+        binding = FragmentRingtoneCreatorBinding.inflate(inflater, container, false)
+        return binding.root
+    }
 }
