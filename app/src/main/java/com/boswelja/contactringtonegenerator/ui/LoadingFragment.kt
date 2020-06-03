9e94f8201c6507d828ecfcecd2f8615189aeba07
Jack Boswell (boswelja) <boswelja@outlook.com>
2020-06-03 14:24:32 +1200

    Create fragment_loading.xml and inflate in LoadingFragment.kt

--- app/src/main/java/com/boswelja/contactringtonegenerator/ui/LoadingFragment.kt
+++ app/src/main/java/com/boswelja/contactringtonegenerator/ui/LoadingFragment.kt
@@ -5,11 +5,15 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
+import com.boswelja.contactringtonegenerator.databinding.FragmentLoadingBinding
 
 class LoadingFragment : Fragment() {
 
+    private lateinit var binding: FragmentLoadingBinding
+
     override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
-        return super.onCreateView(inflater, container, savedInstanceState)
+        binding = FragmentLoadingBinding.inflate(inflater, container, false)
+        return binding.root
     }
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
