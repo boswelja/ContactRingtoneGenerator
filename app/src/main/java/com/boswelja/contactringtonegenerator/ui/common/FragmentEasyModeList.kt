c7d9220a6c705d4b10c5f0c85e79b993cff4bfc2
Jack Boswell (boswelja) <boswelja@outlook.com>
2020-06-04 21:19:02 +1200

    Create a common Fragment class FragmentEasyModeList.kt for using fragment_easy_mode_list.xml layout

---
+++ app/src/main/java/com/boswelja/contactringtonegenerator/ui/common/FragmentEasyModeList.kt
@@ -0,0 +1,35 @@
+package com.boswelja.contactringtonegenerator.ui.common
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.fragment.app.Fragment
+import com.boswelja.contactringtonegenerator.databinding.FragmentEasyModeListBinding
+
+abstract class FragmentEasyModeList : Fragment() {
+
+    private lateinit var binding: FragmentEasyModeListBinding
+
+    open fun onCreateWidgetView(): View? = null
+
+    override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
+        binding = FragmentEasyModeListBinding.inflate(inflater, container, false)
+        val widgetView = onCreateWidgetView()
+        if (widgetView != null) binding.widgetContainer.addView(widgetView, 0)
+        return binding.root
+    }
+
+    protected fun setLoading(loading: Boolean) {
+        binding.apply {
+            if (loading) {
+                loadingSpinner.visibility = View.VISIBLE
+                recyclerView.isEnabled = false
+            } else {
+                loadingSpinner.visibility = View.INVISIBLE
+                recyclerView.isEnabled = true
+            }
+        }
+    }
+
+}
