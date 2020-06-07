024f0dc1852f183809219f53fb509962636a2c8c
Jack Boswell (boswelja) <boswelja@outlook.com>
2020-06-07 18:58:14 +1200

    Remove ContactRingtone.kt Also touch up ContactManager.kt formatting

--- app/src/main/java/com/boswelja/contactringtonegenerator/contacts/ContactManager.kt
+++ app/src/main/java/com/boswelja/contactringtonegenerator/contacts/ContactManager.kt
@@ -7,7 +7,6 @@ import android.net.Uri
 import android.provider.ContactsContract
 import androidx.core.database.getStringOrNull
 
-
 object ContactManager {
 
     private val CONTACTS_PROJECTION = arrayOf(
@@ -53,8 +52,11 @@ object ContactManager {
     }
 
     private fun getContactNickname(context: Context, lookupKey: String): String? {
-        val cursor = context.contentResolver.query(ContactsContract.Data.CONTENT_URI,
-            CONTACT_NICKNAME_PROJECTION, "${ContactsContract.Data.LOOKUP_KEY} = ? AND ${ContactsContract.CommonDataKinds.Nickname.MIMETYPE} = ?", arrayOf(lookupKey, ContactsContract.CommonDataKinds.Nickname.CONTENT_ITEM_TYPE), null)
+        val cursor = context.contentResolver.query(
+                ContactsContract.Data.CONTENT_URI,
+                CONTACT_NICKNAME_PROJECTION,
+                "${ContactsContract.Data.LOOKUP_KEY} = ? AND ${ContactsContract.CommonDataKinds.Nickname.MIMETYPE} = ?",
+                arrayOf(lookupKey, ContactsContract.CommonDataKinds.Nickname.CONTENT_ITEM_TYPE), null)
 
         var nickname: String? = null
         if (cursor != null) {
@@ -68,7 +70,7 @@ object ContactManager {
         return nickname
     }
 
-    fun getContactPhotoUri(context: Context, contactId: Long) : Uri? {
+    private fun getContactPhotoUri(context: Context, contactId: Long) : Uri? {
         val contactUri = ContentUris.withAppendedId(ContactsContract.Contacts.CONTENT_URI, contactId)
         val photoUri = Uri.withAppendedPath(contactUri, ContactsContract.Contacts.Photo.CONTENT_DIRECTORY)
         val cursor = context.contentResolver.query(photoUri, arrayOf(ContactsContract.Contacts.Photo.PHOTO), null, null, null)
--- app/src/main/java/com/boswelja/contactringtonegenerator/contacts/ContactRingtone.kt
+++
@@ -1,9 +0,0 @@
-package com.boswelja.contactringtonegenerator.contacts
-
-import java.io.File
-
-data class ContactRingtone(
-        val contact: Contact,
-        val id: String,
-        val ringtoneFile: File
-)
