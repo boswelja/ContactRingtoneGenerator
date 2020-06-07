8d2d9278637ead11fd9a63809940418592b98a37
Jack Boswell (boswelja) <boswelja@outlook.com>
2020-06-07 20:30:35 +1200

    Create AudioJoiner.kt and AudioJoinJob.kt Currently doesn't actually join files, but class structure was necessary

---
+++ app/src/main/java/com/boswelja/contactringtonegenerator/ringtonegen/AudioJoinJob.kt
@@ -0,0 +1,5 @@
+package com.boswelja.contactringtonegenerator.ringtonegen
+
+import java.io.File
+
+class AudioJoinJob(val id: String, val outFile: File, vararg val inFiles: File)
---
+++ app/src/main/java/com/boswelja/contactringtonegenerator/ringtonegen/AudioJoiner.kt
@@ -0,0 +1,65 @@
+package com.boswelja.contactringtonegenerator.ringtonegen
+
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.MainScope
+import kotlinx.coroutines.launch
+import kotlinx.coroutines.withContext
+
+class AudioJoiner {
+
+    private val jobs = ArrayList<AudioJoinJob>()
+    private val coroutineScope = MainScope()
+
+    private var isJoining: Boolean = false
+
+    var progressListener: JoinProgressListener? = null
+
+    private fun startJoining() {
+        if (!isJoining) {
+            isJoining = true
+            coroutineScope.launch(Dispatchers.IO) {
+                jobs.forEach { job ->
+                    val jobId = job.id
+                    notifyJobStarted(jobId)
+                    // TODO Join files
+                    val success = job.outFile.exists()
+                    if (success) {
+                        notifyJobCompleted(jobId)
+                    } else {
+                        notifyJobFailed(jobId)
+                    }
+                }
+                isJoining = false
+            }
+        }
+    }
+
+    private suspend fun notifyJobStarted(id: String) {
+        withContext(Dispatchers.Main) {
+            progressListener?.onJoinStarted(id)
+        }
+    }
+
+    private suspend fun notifyJobCompleted(id: String) {
+        withContext(Dispatchers.Main) {
+            progressListener?.onJoinCompleted(id)
+        }
+    }
+
+    private suspend fun notifyJobFailed(id: String) {
+        withContext(Dispatchers.Main) {
+            progressListener?.onJoinFailed(id)
+        }
+    }
+
+    fun enqueue(job: AudioJoinJob) {
+        jobs.add(job)
+        startJoining()
+    }
+
+    interface JoinProgressListener {
+        fun onJoinStarted(jobId: String)
+        fun onJoinCompleted(jobId: String)
+        fun onJoinFailed(jobId: String)
+    }
+}
