755a72da651416537b160015cffe7aeda1408456
Jack Boswell (boswelja) <boswelja@outlook.com>
2020-06-15 18:38:12 +1200

    Set up ProgressFragment.kt logic for incrementing the progress bar

--- app/src/main/java/com/boswelja/contactringtonegenerator/ui/ProgressFragment.kt
+++ app/src/main/java/com/boswelja/contactringtonegenerator/ui/ProgressFragment.kt
@@ -6,10 +6,39 @@ import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
 import com.boswelja.contactringtonegenerator.databinding.FragmentLoadingBinding
+import com.boswelja.contactringtonegenerator.ringtonegen.RingtoneGenerator
+import com.boswelja.contactringtonegenerator.tts.SynthesisResult
 
-class ProgressFragment : Fragment() {
+class ProgressFragment : Fragment(), RingtoneGenerator.ProgressListener {
 
     private lateinit var binding: FragmentLoadingBinding
+
+    override fun onGenerateStarted(totalJobCount: Int) {
+        binding.progressBar.apply {
+            max = totalJobCount
+            progress = 0
+            secondaryProgress = 0
+        }
+    }
+
+    override fun onGenerateFinished() {
+
+    }
+
+    override fun onJobStarted() {
+
+    }
+
+    override fun onJobCompleted(success: Boolean, synthesisResult: SynthesisResult) {
+        binding.progressBar.apply {
+            if (success) {
+                progress += 1
+                secondaryProgress += 1
+            } else {
+                secondaryProgress += 1
+            }
+        }
+    }
 
     override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
         binding = FragmentLoadingBinding.inflate(inflater, container, false)
