c06c94f10f612f89d33bf868efcac883e681fe3c
Jack Boswell (boswelja) <boswelja@outlook.com>
2020-06-23 22:02:35 +1200

    Delete voice picker entirely Favor the system settings

--- app/src/main/java/com/boswelja/contactringtonegenerator/ui/voicepicker/VoiceSelectedCallback.kt
+++
@@ -1,9 +0,0 @@
-package com.boswelja.contactringtonegenerator.ui.voicepicker
-
-import android.speech.tts.Voice
-
-interface VoiceSelectedCallback {
-
-    fun onSelected(item: Voice)
-    fun onPreview(item: Voice)
-}
