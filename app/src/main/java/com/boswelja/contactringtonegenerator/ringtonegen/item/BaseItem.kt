fe28c959215311d95be250019aa230e6be707c2d
Jack Boswell (boswelja) <boswelja@outlook.com>
2020-06-24 19:59:21 +1200

    make BaseItem.kt override equals

--- app/src/main/java/com/boswelja/contactringtonegenerator/ringtonegen/item/BaseItem.kt
+++ app/src/main/java/com/boswelja/contactringtonegenerator/ringtonegen/item/BaseItem.kt
@@ -4,4 +4,15 @@ abstract class BaseItem(val id: ID) {
 
     abstract fun getLabel(): String
     abstract fun getEngineText(): String
+
+    override fun equals(other: Any?): Boolean {
+        if (other is BaseItem) {
+            return other.id == id
+        }
+        return super.equals(other)
+    }
+
+    override fun hashCode(): Int {
+        return id.hashCode()
+    }
 }
