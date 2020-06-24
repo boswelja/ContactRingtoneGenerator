a38c85c38ab10ae2651cd08be928eeead5f526cc
Jack Boswell (boswelja) <boswelja@outlook.com>
2020-06-24 14:48:43 +1200

    Create SynthesisResultTest.kt

---
+++ app/src/test/java/com/boswelja/contactringtonegenerator/tts/SynthesisResultTest.kt
@@ -0,0 +1,21 @@
+package com.boswelja.contactringtonegenerator.tts
+
+import org.junit.Test
+
+import org.junit.Assert.* // ktlint-disable
+import java.io.File
+
+class SynthesisResultTest {
+
+    @Test
+    fun testEquals() {
+        val rootFile = File.listRoots().first()
+        val job1 = SynthesisResult("id1", rootFile)
+        val job2 = SynthesisResult("id1", rootFile)
+        val job3 = SynthesisResult("id1", File(rootFile, "child"))
+        val job4 = SynthesisResult("id2", rootFile)
+        assertEquals(job1, job2)
+        assertEquals(job1, job3)
+        assertNotEquals(job1, job4)
+    }
+}
