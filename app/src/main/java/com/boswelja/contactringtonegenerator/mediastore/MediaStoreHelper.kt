52e8288a67006cc75af565915b30f0db45231b5c
Jack Boswell (boswelja) <boswelja@outlook.com>
2020-06-24 17:07:44 +1200

    Delete old contact ringtones before creating a new one if exists

--- app/src/main/java/com/boswelja/contactringtonegenerator/mediastore/MediaStoreHelper.kt
+++ app/src/main/java/com/boswelja/contactringtonegenerator/mediastore/MediaStoreHelper.kt
@@ -18,6 +18,12 @@ object MediaStoreHelper {
         MediaStore.Audio.Media.IS_RINGTONE
     )
 
+    private val RINGTONE_COLLECTION = if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.Q) {
+        MediaStore.Audio.Media.getContentUri(MediaStore.VOLUME_EXTERNAL_PRIMARY)
+    } else {
+        MediaStore.Audio.Media.EXTERNAL_CONTENT_URI
+    }
+
     /**
      * Scan a ringtone file into the [MediaStore].
      * @param context [Context].
@@ -27,18 +33,17 @@ object MediaStoreHelper {
     suspend fun scanNewFile(context: Context, file: File): Uri? {
         return withContext(Dispatchers.IO) {
             val contentResolver = context.contentResolver
-            val ringtoneCollection = if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.Q) {
-                MediaStore.Audio.Media.getContentUri(MediaStore.VOLUME_EXTERNAL_PRIMARY)
-            } else {
-                MediaStore.Audio.Media.EXTERNAL_CONTENT_URI
-            }
             val values = ContentValues().apply {
                 put(MediaStore.Audio.Media.DISPLAY_NAME, file.name)
                 if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.Q) put(MediaStore.Audio.Media.IS_PENDING, 1)
                 put(MediaStore.Audio.Media.MIME_TYPE, "audio/ogg")
                 put(MediaStore.Audio.Media.IS_RINGTONE, true)
             }
-            val uri = contentResolver.insert(ringtoneCollection, values)
+
+            // Delete the ringtone if it already exists
+            deleteRingtones(context, arrayOf(file.name))
+
+            val uri = contentResolver.insert(RINGTONE_COLLECTION, values)
             uri?.let {
                 FileInputStream(file).use { inStream ->
                     contentResolver.openOutputStream(it)?.use { outStream ->
@@ -69,13 +74,8 @@ object MediaStoreHelper {
         Timber.d("deleteAllRingtones() called")
         withContext(Dispatchers.IO) {
             val contentResolver = context.contentResolver
-            val ringtoneCollection = if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.Q) {
-                MediaStore.Audio.Media.getContentUri(MediaStore.VOLUME_EXTERNAL_PRIMARY)
-            } else {
-                MediaStore.Audio.Media.EXTERNAL_CONTENT_URI
-            }
             val displayNameArray = ArrayList<String>()
-            val query = contentResolver.query(ringtoneCollection, DELETE_RINGTONE_PROJECTION, null, null, null)
+            val query = contentResolver.query(RINGTONE_COLLECTION, DELETE_RINGTONE_PROJECTION, null, null, null)
             if (query != null && query.moveToFirst()) {
                 val displayNameCol = query.getColumnIndex(MediaStore.Audio.Media.DISPLAY_NAME)
                 do {
@@ -84,14 +84,26 @@ object MediaStoreHelper {
                     if (displayName.endsWith("-generated-ringtone.ogg")) {
                         displayNameArray.add(displayName)
                     }
-                }
-                while (query.moveToNext())
+                } while (query.moveToNext())
                 query.close()
             } else {
                 Timber.w("Query null or empty")
             }
-            Timber.d("Deleting ${displayNameArray.count()} ringtones")
-            contentResolver.delete(ringtoneCollection, "${MediaStore.Audio.Media.DISPLAY_NAME} = ?", displayNameArray.toTypedArray())
+            deleteRingtones(context, displayNameArray.toTypedArray())
+        }
+    }
+
+    /**
+     * Delete a single ringtone.
+     * @param context [Context].
+     * @param fileNames The file names of all the ringtones to delete.
+     */
+    suspend fun deleteRingtones(context: Context, fileNames: Array<String>) {
+        Timber.d("Deleting ${fileNames.count()} ringtones")
+        try {
+            context.contentResolver.delete(RINGTONE_COLLECTION, "${MediaStore.Audio.Media.DISPLAY_NAME} = ?", fileNames)
+        } catch (e: Exception) {
+            Timber.w(e)
         }
     }
 }
