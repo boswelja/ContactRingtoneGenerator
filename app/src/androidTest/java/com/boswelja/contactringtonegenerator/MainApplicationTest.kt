7b9ce2288b788ad9b0e983272502bfb38bc59e2b
Jack Boswell (boswelja) <boswelja@outlook.com>
2020-06-24 12:43:06 +1200

    Create MainApplicationTest.kt

---
+++ app/src/androidTest/java/com/boswelja/contactringtonegenerator/MainApplicationTest.kt
@@ -0,0 +1,14 @@
+package com.boswelja.contactringtonegenerator
+
+import org.junit.Assert.* // ktfmt-disable
+import org.junit.Test
+import timber.log.Timber
+
+class MainApplicationTest {
+
+    @Test
+    fun onCreate() {
+        val expectedTreeCount = if (BuildConfig.DEBUG) 1 else 0
+        assertEquals(Timber.treeCount(), expectedTreeCount)
+    }
+}
