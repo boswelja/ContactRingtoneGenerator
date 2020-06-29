4a6ec3ad421831f23a8b6338d804b19c0bc22b5c
Jack Boswell (boswelja) <boswelja@outlook.com>
2020-06-29 22:34:05 +1200

    Create WizardDataViewModel.kt

---
+++ app/src/main/java/com/boswelja/contactringtonegenerator/ui/WizardDataViewModel.kt
@@ -0,0 +1,16 @@
+package com.boswelja.contactringtonegenerator.ui
+
+import android.content.Context
+import androidx.lifecycle.ViewModel
+import com.boswelja.contactringtonegenerator.contacts.Contact
+import com.boswelja.contactringtonegenerator.ringtonegen.RingtoneGenerator
+import com.boswelja.contactringtonegenerator.ringtonegen.item.BaseItem
+
+class WizardDataViewModel : ViewModel() {
+
+    val selectedContacts = ArrayList<Contact>()
+    val ringtoneItems = ArrayList<BaseItem>()
+
+    fun createRingtoneGenerator(context: Context): RingtoneGenerator =
+            RingtoneGenerator(context, ringtoneItems, selectedContacts)
+}
