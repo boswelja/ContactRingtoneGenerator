a6e660a3d79ed2dbe5eee0886ec7cd49b53ef8fa
Jack Boswell (boswelja) <boswelja@outlook.com>
2021-05-10 14:26:48 +1200

    Remove Ringtone creator adapter logic

--- app/src/main/java/com/boswelja/contactringtonegenerator/ui/ringtonecreator/adapter/ActionClickCallback.kt
+++
@@ -1,8 +0,0 @@
-package com.boswelja.contactringtonegenerator.ui.ringtonecreator.adapter
-
-import com.boswelja.contactringtonegenerator.ringtonegen.item.ID
-import com.boswelja.contactringtonegenerator.ringtonegen.item.common.StructureItem
-
-class ActionClickCallback(val callback: (id: ID, position: Int) -> Unit) {
-    fun onClick(item: StructureItem, position: Int) = callback(item.id, position)
-}
