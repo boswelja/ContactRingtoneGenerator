263980f6a500b5ea430a9dc007099402465dc384
Jack Boswell (boswelja) <boswelja@outlook.com>
2021-07-06 16:32:29 +1200

    Don't plant Timber tree on release

--- app/src/main/kotlin/com/boswelja/contactringtonegenerator/MainApplication.kt
+++ app/src/main/kotlin/com/boswelja/contactringtonegenerator/MainApplication.kt
@@ -7,6 +7,6 @@ class MainApplication : Application() {
 
     override fun onCreate() {
         super.onCreate()
-        Timber.plant(Timber.DebugTree())
+        if (BuildConfig.DEBUG) Timber.plant(Timber.DebugTree())
     }
 }
