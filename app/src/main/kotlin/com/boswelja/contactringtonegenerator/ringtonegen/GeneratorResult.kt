76066b6776b2070a0f0a0fd2b2c9e93f03a0877d
Jack Boswell (boswelja) <boswelja@outlook.com>
2021-07-12 21:17:55 +1200

    Create GeneratorResultData

--- app/src/main/kotlin/com/boswelja/contactringtonegenerator/ringtonegen/GeneratorResult.kt
+++ app/src/main/kotlin/com/boswelja/contactringtonegenerator/ringtonegen/GeneratorResult.kt
@@ -5,3 +5,15 @@ enum class GeneratorResult {
     MIXED,
     SUCCESSFUL
 }
+
+/**
+ * A data class containing two sets of
+ * [com.boswelja.contactringtonegenerator.contactpicker.Contact.lookupKey]s. One for contacts whose
+ * ringtone was successfully generated, and one for contacts whose ringtone failed to generate.
+ * @param successfulContacts The [Set] of lookupKeys for contacts whose ringtone was generated.
+ * @param failedContacts The [Set] of lookupKeys for contacts whose ringtone was not generated.
+ */
+data class GeneratorResultData(
+    val successfulContacts: Set<String>,
+    val failedContacts: Set<String>
+)
